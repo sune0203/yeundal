b62442cf572f9a835f911607f5f88a8dca0f2dc9
Rhats <Rhats@192.168.1.113>
2020-07-17 15:32:10 +0900

    연산 달인

---
+++ src/MainClass.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import sub.Calculator;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculator yd = new Calculator();
+		yd.input();
+		yd.rdata();
+		yd.calcu();
+		
+		
+	}
+	//1. 더하기
+	//2. 빼기
+	//3. 곱하기
+	//4. 나누기
+	//0. 종료
+	//1 > 더하기 문제가 10 문제 정도 나오고
+	
+	// 정답수 와 틀린수 를 출력 해주고 다시 위의 메뉴 출력
+	// 그외 연산도 위와 같은 방식으로
+	// 문자가 입력되면 예외처리까지 해보세요
+	// 메인 메소드에는 최소한의 코드만 있고 For(;;)
+	
+	// 다 클래스에 메소드로 구현 해보세요
+	// 나중에 짤라내기 하는게 편할수 있다.
+}
---
+++ src/sub/Calculator.java
@@ -0,0 +1,103 @@
+package sub;
+import java.util.*;
+
+public class Calculator {
+	int input ,result;
+	Random rd = new Random();
+	Scanner sc = new Scanner(System.in);
+	ArrayList<Integer> list = new ArrayList<Integer>();
+	
+	public void rdata() {
+		for(int i = 0; i < 20; i++) {
+			this.list.add(rd.nextInt(89)+10);
+//			System.out.println(list.get(i));
+		}
+	}
+	
+	public void calcu() {
+		int O=0, X=0;
+		for(;;) {
+			int i=0;
+			O=0;
+			X=0;
+			if(input == 1) {
+				for(int r=0; r<list.size()/2; r++) {
+					result = list.get(i)+list.get(i+1);
+					System.out.print(list.get(i)+" + "+list.get(i+1)+ " = ");
+					int dab = sc.nextInt();
+					if(result == dab) {
+						i+=2;
+						O++;
+					}else {
+						i+=2;
+						X++;
+					}
+				}
+			}else if(input == 2) {
+				for(int r=0; r<list.size()/2; r++) {
+					result = list.get(i)-list.get(i+1);
+					System.out.print(list.get(i)+" - "+list.get(i+1)+ " = ");
+					int dab = sc.nextInt();
+					if(result == dab) {
+						i++;
+						O++;
+					}else {
+						X++;
+						i++;
+					}
+				}
+			}else if(input == 3) {
+				for(int r=0; r<list.size()/2; r++) {
+					result = list.get(i)*list.get(i+1);
+					System.out.print(list.get(i)+" X "+list.get(i+1)+ " = ");
+					int dab = sc.nextInt();
+					if(result == dab) {
+						i++;
+						O++;
+					}else {
+						X++;
+						i++;
+					}
+				}
+			}else if(input == 4) {
+				for(int r=0; r<list.size()/2; r++) {
+					result = list.get(i)/list.get(i+1);
+					System.out.print(list.get(i)+" / "+list.get(i+1)+ " = ");
+					int dab = sc.nextInt();
+					if(result == dab) {
+						i++;
+						O++;
+					}else {
+						X++;
+						i++;
+					}
+				}
+			}else {
+				break;
+			}
+			input();
+			if(this.input == 0) {
+				break;
+			}
+		}
+		System.out.println("O = "+O+" X = "+X);
+	}
+	
+	public void input() {
+		for(;;) {
+			try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("원하는 연산을 입력하세요. ");
+			System.out.println("1. 더하기 2. 빼기 3. 곱하기 4. 나누기 0.종료");
+			input = sc.nextInt();
+			break;
+			}
+			catch (Exception e) {
+				System.out.println("오류 발생 !!");
+				// TODO: handle exception
+			}
+		}
+	}
+	
+	
+}
